bd8095b8f4532bbf82dc77a436681aab69facc10
Rifan Setiadi <rfan2442@gmail.com>
2024-11-12 01:36:57 +0700

    feat: add WidgetsFlutterBinding.ensureInitialized();

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const QRScanner());
 }
 
